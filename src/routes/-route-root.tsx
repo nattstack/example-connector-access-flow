d72341c117b6d119db60af7bf78db276772e5fcd
Natt <natt.lefevre@gmail.com>
2026-08-25 05:42:54 -0700

    feat: introduce Providers component to wrap application with TooltipProvider for enhanced tooltip functionality

--- src/routes/-route-root.tsx
+++ src/routes/-route-root.tsx
@@ -2,16 +2,17 @@ import { HeadContent, Outlet } from "@tanstack/react-router"
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools"
 import type { JSX } from "react"
 import { createPortal } from "react-dom"
+import { Providers } from "#/components/providers"
 import { ENVIRONMENT } from "#/utils/environment"
 
 export function RouteRoot(): JSX.Element {
   return (
-    <>
+    <Providers>
       {createPortal(<HeadContent />, document.head)}
 
       <Outlet />
 
       {ENVIRONMENT.IS_DEVELOPMENT && <TanStackRouterDevtools position="bottom-right" />}
-    </>
+    </Providers>
   )
 }
