88d2f04129d999cd4efef1fa267ded5b4ebe5b4f
Natt <natt.lefevre@gmail.com>
2026-08-25 00:01:33 -0700

    fix: update production base URL to new deployment on Vercel

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -2,7 +2,7 @@ import { ENVIRONMENT } from "#/utils/environment"
 
 const BASE_URLS = {
   DEVELOPMENT: "https://connector.localhost",
-  PRODUCTION: "https://kale.so",
+  PRODUCTION: "https://something.vercel.app",
 }
 
 export const BASE_URL = ENVIRONMENT.IS_PRODUCTION ? BASE_URLS.PRODUCTION : BASE_URLS.DEVELOPMENT
