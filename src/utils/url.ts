30def0b87ba5b51414f83d2c2120c6b07c1602e6
Natt <natt.lefevre@gmail.com>
2026-08-25 17:11:42 -0700

    refactor: rename BASE_URLS to BASE_URL_MAP for clarity; update BASE_URL assignment to improve readability

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,8 +1,10 @@
 import { ENVIRONMENT } from "#/utils/environment"
 
-const BASE_URLS = {
+const BASE_URL_MAP = {
   DEVELOPMENT: "https://connector.localhost",
   PRODUCTION: "https://connector-access-flow.vercel.app",
 }
 
-export const BASE_URL = ENVIRONMENT.IS_PRODUCTION ? BASE_URLS.PRODUCTION : BASE_URLS.DEVELOPMENT
+export const BASE_URL = ENVIRONMENT.IS_PRODUCTION
+  ? BASE_URL_MAP.PRODUCTION
+  : BASE_URL_MAP.DEVELOPMENT
